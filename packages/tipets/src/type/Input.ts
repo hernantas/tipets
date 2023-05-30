4318e4844579e727ab1d78ac7c3c98154321100f
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2023-05-30 19:58:25 +0700

    doc: add ignore warning

--- packages/tipets/src/type/Input.ts
+++ packages/tipets/src/type/Input.ts
@@ -2,5 +2,6 @@ import { inputSymbol } from '../symbol/inputSymbol'
 
 /** Interface used to statically represent input type */
 export interface Input<T = unknown> {
+  /** This should be ignored. Implementation should ignore using `!` operator */
   readonly [inputSymbol]: T
 }
--- packages/tipets/src/type/Type.ts
+++ packages/tipets/src/type/Type.ts
@@ -2,5 +2,6 @@ import { typeSymbol } from '../symbol/typeSymbol'
 
 /** Interface used to statically represent type */
 export interface Type<T = unknown> {
+  /** This should be ignored. Implementation should ignore using `!` operator */
   readonly [typeSymbol]: T
 }
