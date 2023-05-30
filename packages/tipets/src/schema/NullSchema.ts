2f452e7e0a489203f10a748a290a5212ec3c362a
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2023-05-30 21:56:56 +0700

    refactor: add `Null` schema

---
+++ packages/tipets/src/schema/NullSchema.ts
@@ -0,0 +1,7 @@
+import { Schema } from './Schema'
+
+export class NullSchema extends Schema<undefined> {
+  public override is(value: unknown): value is undefined {
+    return value === null
+  }
+}
