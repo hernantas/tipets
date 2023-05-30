2783689eef8f0e5b1d527c360f6d4c5eeae2939c
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2023-05-30 21:07:34 +0700

    refactor: add `Number` schema

---
+++ packages/tipets/src/schema/NumberSchena.test.ts
@@ -0,0 +1,66 @@
+import { number } from './builder/number'
+
+describe('Number Schema', () => {
+  const schema = number()
+
+  it('Is', () => {
+    expect(schema.is(0)).toBe(true)
+    expect(schema.is(255)).toBe(true)
+    expect(schema.is(-255)).toBe(true)
+    expect(schema.is(255.0)).toBe(true)
+    expect(schema.is(0xff)).toBe(true)
+    expect(schema.is(0b11111111)).toBe(true)
+    expect(schema.is(0.255e3)).toBe(true)
+    expect(schema.is(NaN)).toBe(true)
+    expect(schema.is(Infinity)).toBe(true)
+    expect(schema.is(-Infinity)).toBe(true)
+    expect(schema.is('0')).toBe(false)
+    expect(schema.is('3')).toBe(false)
+  })
+
+  describe('Validation', () => {
+    it('Min', () => {
+      const validator = schema.min(0)
+      expect(validator.validate(0)).toHaveLength(0)
+      expect(validator.validate(1)).toHaveLength(0)
+      expect(validator.validate(Number.MAX_SAFE_INTEGER)).toHaveLength(0)
+      expect(validator.validate(-1).length).toBeGreaterThan(0)
+      expect(
+        validator.validate(Number.MIN_SAFE_INTEGER).length
+      ).toBeGreaterThan(0)
+    })
+
+    it('Max', () => {
+      const validator = schema.max(0)
+      expect(validator.validate(0)).toHaveLength(0)
+      expect(validator.validate(-1)).toHaveLength(0)
+      expect(validator.validate(Number.MIN_SAFE_INTEGER)).toHaveLength(0)
+      expect(validator.validate(1).length).toBeGreaterThan(0)
+      expect(
+        validator.validate(Number.MAX_SAFE_INTEGER).length
+      ).toBeGreaterThan(0)
+    })
+
+    it('Greater', () => {
+      const validator = schema.greater(0)
+      expect(validator.validate(1)).toHaveLength(0)
+      expect(validator.validate(Number.MAX_SAFE_INTEGER)).toHaveLength(0)
+      expect(validator.validate(0).length).toBeGreaterThan(0)
+      expect(validator.validate(-1).length).toBeGreaterThan(0)
+      expect(
+        validator.validate(Number.MIN_SAFE_INTEGER).length
+      ).toBeGreaterThan(0)
+    })
+
+    it('Less', () => {
+      const validator = schema.less(0)
+      expect(validator.validate(-1)).toHaveLength(0)
+      expect(validator.validate(Number.MIN_SAFE_INTEGER)).toHaveLength(0)
+      expect(validator.validate(0).length).toBeGreaterThan(0)
+      expect(validator.validate(1).length).toBeGreaterThan(0)
+      expect(
+        validator.validate(Number.MAX_SAFE_INTEGER).length
+      ).toBeGreaterThan(0)
+    })
+  })
+})
