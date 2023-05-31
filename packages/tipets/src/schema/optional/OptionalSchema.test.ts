988bb5bcd2ad34ca48782d3b175ea18d78c6a21d
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2023-05-31 22:50:56 +0700

    fix: `OptionalSchema` not accepting `undefined`

--- packages/tipets/src/schema/optional/OptionalSchema.test.ts
+++ packages/tipets/src/schema/optional/OptionalSchema.test.ts
@@ -7,12 +7,14 @@ describe('Optional Schema', () => {
   it('Type Guard', () => {
     expect(schema.is('Hello')).toBe(true)
     expect(schema.is(undefined)).toBe(true)
+    expect(schema.is(null)).toBe(false)
     expect(schema.is(0)).toBe(false)
     expect(schema.is(false)).toBe(false)
   })
 
   it('Validation', () => {
     expect(schema.validate('Hello')).toHaveLength(0)
+    expect(schema.validate(undefined)).toHaveLength(0)
     expect(schema.validate('Hell')).toHaveLength(2)
   })
 })
--- packages/tipets/src/schema/optional/OptionalSchema.ts
+++ packages/tipets/src/schema/optional/OptionalSchema.ts
@@ -4,18 +4,18 @@ import { Schema } from '../Schema'
 import { OptionalDefinition } from './OptionalDefinition'
 
 export class OptionalSchema<T extends Schema> extends Schema<
-  TypeOf<T>,
+  TypeOf<T> | undefined,
   OptionalDefinition<T>
 > {
   public get type(): T {
     return this.get('type')
   }
 
-  public override is(value: unknown): value is TypeOf<T> {
+  public override is(value: unknown): value is TypeOf<T> | undefined {
     return value === undefined || this.type.is(value)
   }
 
-  public override validate(value: TypeOf<T>): Violation[] {
+  public override validate(value: TypeOf<T> | undefined): Violation[] {
     return super
       .validate(value)
       .concat(...(this.type.is(value) ? this.type.validate(value) : []))
