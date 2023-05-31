d781444a7cf1e9a842902f1f1888d9dbaec73864
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2023-05-31 15:02:25 +0700

    refactor: add `Literal` schema

---
+++ packages/tipets/src/schema/definition/LiteralDefinition.ts
@@ -0,0 +1,8 @@
+import { LiteralType } from '../../alias/LiteralType'
+import { Definition } from './Definition'
+
+export interface LiteralDefinition<T extends LiteralType>
+  extends Definition<T> {
+  /** Literal value */
+  readonly value: T
+}
