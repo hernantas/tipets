2534c81bb646ac98c91d419c4ca58b349a329346
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2023-05-31 17:31:23 +0700

    refactor: simplify `LiteralSchema` test

--- packages/tipets/src/schema/literal/LiteralSchema.test.ts
+++ packages/tipets/src/schema/literal/LiteralSchema.test.ts
@@ -1,39 +1,33 @@
 import { literal } from './literal'
 
 describe('Literal Schema', () => {
-  describe('String', () => {
+  it('Is (String)', () => {
     const schema = literal('true')
-    it('Is', () => {
-      expect(schema.is('true')).toBe(true)
-      expect(schema.is('false')).toBe(false)
-      expect(schema.is(true)).toBe(false)
-      expect(schema.is(false)).toBe(false)
-      expect(schema.is(0)).toBe(false)
-      expect(schema.is(1)).toBe(false)
-    })
+    expect(schema.is('true')).toBe(true)
+    expect(schema.is('false')).toBe(false)
+    expect(schema.is(true)).toBe(false)
+    expect(schema.is(false)).toBe(false)
+    expect(schema.is(0)).toBe(false)
+    expect(schema.is(1)).toBe(false)
   })
 
-  describe('Number', () => {
+  it('Is (Number)', () => {
     const schema = literal(0)
-    it('Is', () => {
-      expect(schema.is(0)).toBe(true)
-      expect(schema.is(1)).toBe(false)
-      expect(schema.is('true')).toBe(false)
-      expect(schema.is('false')).toBe(false)
-      expect(schema.is(true)).toBe(false)
-      expect(schema.is(false)).toBe(false)
-    })
+    expect(schema.is(0)).toBe(true)
+    expect(schema.is(1)).toBe(false)
+    expect(schema.is('true')).toBe(false)
+    expect(schema.is('false')).toBe(false)
+    expect(schema.is(true)).toBe(false)
+    expect(schema.is(false)).toBe(false)
   })
 
-  describe('Boolean', () => {
+  it('Is (Boolean)', () => {
     const schema = literal(true)
-    it('Is', () => {
-      expect(schema.is(true)).toBe(true)
-      expect(schema.is(false)).toBe(false)
-      expect(schema.is('true')).toBe(false)
-      expect(schema.is('false')).toBe(false)
-      expect(schema.is(0)).toBe(false)
-      expect(schema.is(1)).toBe(false)
-    })
+    expect(schema.is(true)).toBe(true)
+    expect(schema.is(false)).toBe(false)
+    expect(schema.is('true')).toBe(false)
+    expect(schema.is('false')).toBe(false)
+    expect(schema.is(0)).toBe(false)
+    expect(schema.is(1)).toBe(false)
   })
 })
