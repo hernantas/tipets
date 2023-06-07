71048ae075d812f95cb4eca4e174494b36473e9f
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2023-06-07 11:11:27 +0700

    refactor: add `NumberCodec`

---
+++ packages/tipets/src/parser/codec/NumberCodec.ts
@@ -0,0 +1,27 @@
+import { NumberSchema } from '../../schema/number/NumberSchema'
+import { Codec } from '../Codec'
+
+export class NumberCodec implements Codec<NumberSchema> {
+  public decode(value: unknown): number {
+    if (typeof value === 'number') {
+      return value
+    }
+
+    if (value === undefined) {
+      return 0
+    }
+
+    if (value === 'NaN') {
+      return NaN
+    } else if (value === '-NaN') {
+      return -NaN
+    }
+
+    // try automatic conversion
+    return Number(value)
+  }
+
+  public encode(value: number): unknown {
+    return value
+  }
+}
