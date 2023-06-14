a793a2800755e640fb05f7f7694379c50b12c2f7
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2023-06-14 12:14:27 +0700

    fix: misleading error message

--- packages/tipets/src/parser/UnsupportedValueError.ts
+++ packages/tipets/src/parser/UnsupportedValueError.ts
@@ -8,7 +8,7 @@ export class UnsupportedValueError extends Error {
   public override readonly name: string = 'UnsupportedValueError'
 
   public constructor(public readonly value: unknown) {
-    super(`Cannot parse given value type`)
+    super(`Cannot parse given value`)
   }
 
   public toViolation(): Violation {
