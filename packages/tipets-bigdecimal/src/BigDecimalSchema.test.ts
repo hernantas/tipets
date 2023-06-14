5933ee8b30d7c83c6a6f937ec6318216c9d25c9e
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2023-06-14 15:39:39 +0700

    refactor(bigdecimal): add `BigDecimalSchema`

---
+++ packages/tipets-bigdecimal/src/BigDecimalSchema.test.ts
@@ -0,0 +1,59 @@
+import { Big } from 'bigdecimal.js'
+import { BigDecimalSchema } from './BigDecimalSchema'
+
+describe('BigDecimalSchema', () => {
+  const schema = BigDecimalSchema.create()
+
+  it('Instance Checking', () => {
+    expect(BigDecimalSchema.is(schema)).toBe(true)
+  })
+
+  it('Type Guard', () => {
+    expect(schema.is(Big('0'))).toBe(true)
+    expect(schema.is(0)).toBe(false)
+  })
+
+  describe('Validation', () => {
+    it('Min', () => {
+      const validator = schema.min(Big('0'))
+      expect(validator.validate(Big(0))).toHaveLength(0)
+      expect(validator.validate(Big(1))).toHaveLength(0)
+      expect(validator.validate(Big(-1))).toHaveLength(1)
+    })
+
+    it('Max', () => {
+      const validator = schema.max(Big('0'))
+      expect(validator.validate(Big(0))).toHaveLength(0)
+      expect(validator.validate(Big(-1))).toHaveLength(0)
+      expect(validator.validate(Big(1))).toHaveLength(1)
+    })
+
+    it('Greater Than', () => {
+      const validator = schema.greater(Big('0'))
+      expect(validator.validate(Big(1))).toHaveLength(0)
+      expect(validator.validate(Big(0))).toHaveLength(1)
+      expect(validator.validate(Big(-1))).toHaveLength(1)
+    })
+
+    it('Lower Than', () => {
+      const validator = schema.less(Big('0'))
+      expect(validator.validate(Big(-1))).toHaveLength(0)
+      expect(validator.validate(Big(0))).toHaveLength(1)
+      expect(validator.validate(Big(1))).toHaveLength(1)
+    })
+
+    it('Positive', () => {
+      const validator = schema.positive()
+      expect(validator.validate(Big(1))).toHaveLength(0)
+      expect(validator.validate(Big(0))).toHaveLength(1)
+      expect(validator.validate(Big(-1))).toHaveLength(1)
+    })
+
+    it('Negative', () => {
+      const validator = schema.negative()
+      expect(validator.validate(Big(-1))).toHaveLength(0)
+      expect(validator.validate(Big(0))).toHaveLength(1)
+      expect(validator.validate(Big(1))).toHaveLength(1)
+    })
+  })
+})
---
+++ packages/tipets-bigdecimal/src/BigDecimalSchema.ts
@@ -0,0 +1,159 @@
+import { BigDecimal } from 'bigdecimal.js'
+import { Schema, kindSymbol, Signature } from 'tipets'
+
+export class BigDecimalSchema extends Schema<BigDecimal> {
+  public static readonly [kindSymbol]: string = 'big-decimal'
+
+  private static readonly instance = new BigDecimalSchema({
+    signature: BigDecimalSchema.signature(),
+  })
+
+  public override readonly [kindSymbol]: string = BigDecimalSchema[kindSymbol]
+
+  /**
+   * Check if given schema is instance of {@link BigDecimalSchema}
+   *
+   * @param schema Schema to be checked
+   * @returns True if schema is instance of {@link BigDecimalSchema}, false
+   *   otherwise
+   */
+  public static override is(schema: Schema): schema is BigDecimalSchema {
+    return schema[kindSymbol] === BigDecimalSchema[kindSymbol]
+  }
+
+  /**
+   * Create new signature for {@link BigDecimalSchema}
+   *
+   * @returns A new signature instance
+   */
+  public static signature(): Signature {
+    return Signature.create('BigDecimal')
+  }
+
+  /**
+   * Create new instance of {@link BigDecimalSchema}
+   *
+   * @returns A new instance of {@link BigDecimalSchema}
+   */
+  public static create(): BigDecimalSchema {
+    return BigDecimalSchema.instance
+  }
+
+  public override is(value: unknown): value is BigDecimal {
+    return value instanceof BigDecimal
+  }
+
+  /**
+   * Add new validation constraint that check minimum number (`<=`)
+   *
+   * @param limit Minimum number
+   * @param message Optional message when rule is violated
+   * @returns A new instance with new rules added
+   */
+  public min(
+    limit: BigDecimal,
+    message: string = `must greater than or equal to ${limit.toPlainString()}`
+  ): this {
+    return this.check({
+      type: `bigdecimal.min`,
+      args: { limit },
+      validate: (v) => v.greaterThanOrEquals(limit),
+      message,
+    })
+  }
+
+  /**
+   * Add new validation constraint that check maximum number (`<=`)
+   *
+   * @param limit Maximum number
+   * @param message Optional message when rule is violated
+   * @returns A new instance with new rules added
+   */
+  public max(
+    limit: BigDecimal,
+    message: string = `must be less than or equal to ${limit.toPlainString()}`
+  ): this {
+    return this.check({
+      type: `bigdecimal.max`,
+      args: { limit },
+      validate: (v) => v.lowerThanOrEquals(limit),
+      message,
+    })
+  }
+
+  /**
+   * Add new validation constraint that check number to be greater than given
+   * number (`>`)
+   *
+   * @param limit Limit number
+   * @param message Optional message when rule is violated
+   * @returns A new instance with new rules added
+   */
+  public greater(
+    limit: BigDecimal,
+    message: string = `must be greater than ${limit.toPlainString()}`
+  ): this {
+    return this.check({
+      type: `bigdecimal.greater`,
+      args: { limit },
+      validate: (v) => v.greaterThan(limit),
+      message,
+    })
+  }
+
+  /**
+   * Add new validation constraint that check number to be less than given
+   * number (`<`)
+   *
+   * @param limit Limit number
+   * @param message Optional message when rule is violated
+   * @returns A new instance with new rules added
+   */
+  public less(
+    limit: BigDecimal,
+    message: string = `${
+      this.get('label') ?? ''
+    } must be less than ${limit.toPlainString()}`
+  ): this {
+    return this.check({
+      type: `bigdecimal.less`,
+      args: { limit },
+      validate: (v) => v.lowerThan(limit),
+      message,
+    })
+  }
+
+  /**
+   * Add new validation constraint that number must be positive
+   *
+   * @param limit Limit number
+   * @param message Optional message when rule is violated
+   * @returns A new instance with new rules added
+   */
+  public positive(
+    message: string = `${this.get('label') ?? ''} must be positive number`
+  ): this {
+    return this.check({
+      type: `bigdecimal.positive`,
+      validate: (v) => v.greaterThan(0),
+      message,
+    })
+  }
+
+  /**
+   * Add new validation constraint that number must be negative
+   *
+   * @param limit Limit number
+   * @param message Optional message when rule is violated
+   * @returns A new instance with new rules added
+   */
+  public negative(
+    message: string = `${this.get('label') ?? ''} must be negative number`
+  ): this {
+    return this.check({
+      type: `bigdecimal.negative`,
+      validate: (v) => v.lowerThan(-0),
+      message,
+    })
+  }
+}
